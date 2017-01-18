9be3fcb2c41b4e44820ce25127adc1bc45f9c481
Becca Krouse <rebecca_zabel@rhoworld.com>
2017-01-18 15:27:59 -0500

    allow custom settings for AE Table

--- R/aeExplorer.R
+++ R/aeExplorer.R
@@ -2,25 +2,74 @@
 #'
 #' This function creates an AE Explorer using R htmlwidgets.  
 #'
-#' @param data A data.frame containing the Adverse Events data.  Required columns include 
-#' `USUBJID` (unique subject ID), `AEBODSYS` (higher-level term), `AEDECOD` (lower-level term), 
-#' and `ARM` (treatment group),
+#' @param data  A data frame containing the Adverse Events data.  
+#' @param id   Participant ID variable name. Default is \code{"USUBJID"}.
+#' @param major Higher-level term variable name.  Default is \code{"AEBODSYS"}.
+#' @param minor Lower-level term variable name.  Default is \code{"AEDECOD"}. 
+#' @param group  Group variable name, each value of which displays in its own column in the AE table unless argument \code{groups} is defined. Default is \code{"ARM"}.
+#' @param details An optional vector of variables to display in the detail listing.  If left as \code{NULL} or unspecified, all variables in input data will appear in detail listing.
+#' @param groups An optional character vector specifying which groups to display. If left as \code{NULL} or unspecified, all classes from \code{group} variable will be shown
+#' @param filters  An optional list containing individual lists for each desired filter.  
+#' To specify a  filter, two arguments (\code{value_col} and \code{label}) are required.   
 #' 
-#' @param width Width in pixels 
-#' @param height Height in pixels  
+#' (e.g. \code{filters = list(list(value_col = "AESER", label = "Serious?"), list(value_col = "SEX", label = "Participant Sex"))})
+#' @param totalCol Specify whether or not to render a total column. Accepts \code{"Show"} (default) or \code{"Hide"}. 
+#' @param diffCol Specify whether or not to render a column of graphical differences. Accepts \code{"Show"} (default) or \code{"Hide"}. 
+#' @param prefTerms Specify whether or not to initially display all lower-level rows. Accepts \code{"Show"} or \code{"Hide"} (default).  
+#' @param maxPrevalence Filters out any higher- and lower-level rows without at least one group rate above specified value. Default is \code{0}.
+#' @param width Width in pixels.
+#' @param height Height in pixels.
 #' @param elementId The element ID for the widget.
 #'
+#' @examples
+#' \dontrun{
+#' # Run AE Explorer with defaults
+#' aeExplorer(data=AE)
+#' 
+#' # Run AE Explorer specifying some parameters
+#' aeExplorer(data=AE, group="SEX", filters=list(list(value_col = "ARM", label = "Treatment Arm")), diffCol="Hide")
+#' }
+#'    
 #' @seealso aeTimelines, safetyHistogram, safetyOutlierExplorer, safetyResultsOverTime, safetyShiftPlot
 #' @source AE Explorer: \url{https://github.com/RhoInc/aeexplorer}.
 #' 
 #' @import htmlwidgets
 #'
 #' @export
-aeExplorer <- function(data, width = NULL, height = NULL, elementId = NULL) {
-
+aeExplorer <- function(data, 
+                       id = 'USUBJID',
+                       major = 'AEBODSYS',
+                       minor = 'AEDECOD',
+                       group = 'ARM',
+                       details = NULL, 
+                       groups = NULL, 
+                       filters = NULL, 
+                       totalCol = 'Show',
+                       diffCol = 'Show',
+                       prefTerms = 'Hide',
+                       maxPrevalence = 0,
+                       width = NULL, height = NULL, elementId = NULL) {
+  
   # forward options using x
   x = list(
-    data=data
+    data=data,
+    settings=jsonlite::toJSON(
+      list(
+      variables=list(id=id,
+                     major=major,
+                     minor=minor,
+                     group=group,
+                     details=I(details)
+                    ),
+       groups=I(groups),
+       filters=I(filters),
+       defaults=list(totalCol=totalCol,
+                     diffCol=diffCol,
+                     prefTerms=prefTerms,
+                     maxPrevalence=maxPrevalence)
+    ),
+    null="null", auto_unbox=T
+    )
   )
 
   # create widget
--- inst/htmlwidgets/aeExplorer.js
+++ inst/htmlwidgets/aeExplorer.js
@@ -12,11 +12,12 @@ HTMLWidgets.widget({
       // renderValue delivers data & settings to the DOM element
       // x = data & settings
       renderValue: function(x) {
+        
+        var settings = x.settings;
 
-          x.data = HTMLWidgets.dataframeToD3(x.data);
+        x.data = HTMLWidgets.dataframeToD3(x.data);
 
-        //  aeTable(el, settings).init(x.data);
-          aeTable(el).init(x.data);
+        aeTable(el, settings).init(x.data);
       },
 
       resize: function(width, height) {
