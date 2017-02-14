29a36ca5f9437aecb349a01cc2d4a904cfbe7cd6
Becca Krouse <rebecca_zabel@rhoworld.com>
2017-02-14 14:19:53 -0500

    update AE Table app

--- R/runAeTableApp.R
+++ R/runAeTableApp.R
@@ -1,7 +1,7 @@
 #' Adverse Events Table Shiny App
 #'
 #' @export
-runSafetyApp <- function() {
+runAeTableApp <- function() {
   appDir <- system.file("shiny-examples", "aeTableApp", package = "safetyexploreR")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)
