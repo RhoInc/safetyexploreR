6b699e45661f248de5d98725c0f5c993718a312a
bzkrouse <rebecca_zabel@rhoworld.com>
2017-04-20 08:27:42 -0400

    update path to sample data

--- data-raw/ADAE.R
+++ data-raw/ADAE.R
@@ -1,4 +1,4 @@
-URL <- "https://raw.githubusercontent.com/RhoInc/viz-library/master/examples/0000-sample-data/safetyData/ADAE.csv"
+URL <- "https://raw.githubusercontent.com/RhoInc/viz-library/master/data/safetyData/ADAE.csv"
 download.file(url=URL,
               destfile='data-raw/ADAE.csv', method='auto')
 
--- data-raw/ADBDS.R
+++ data-raw/ADBDS.R
@@ -1,4 +1,4 @@
-URL <- "https://raw.githubusercontent.com/RhoInc/viz-library/master/examples/0000-sample-data/safetyData/ADBDS.csv"
+URL <- "https://raw.githubusercontent.com/RhoInc/viz-library/master/data/safetyData/ADBDS.csv"
 download.file(url=URL,
               destfile='data-raw/ADBDS.csv', method='auto')
 
