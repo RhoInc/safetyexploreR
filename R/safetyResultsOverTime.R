577e91b6d482b100359095f6103796d566cd2e01
bzkrouse <rebecca_zabel@rhoworld.com>
2018-03-02 16:27:07 -0500

    Update defaults and add arg for time_order_col

--- R/safetyResultsOverTime.R
+++ R/safetyResultsOverTime.R
@@ -4,8 +4,9 @@
 #'
 #' @param data A data frame containing the labs data. 
 #' @param id_col   Participant ID variable name. Default is \code{"USUBJID"}.
-#' @param time_col Timing of collection variable name. Default is \code{c("VISITN")}.
-#' @param time_label Label of variable specified in \code{time_col}. Defaults to \code{c('Visit Number')}. If set to \code{NULL}, variable name will be used for labels.
+#' @param time_col Timing of collection variable name. Default is \code{c("VISIT")}.
+#' @param time_label Label of variable specified in \code{time_col}. Defaults to \code{c('Visit')}. If set to \code{NULL}, variable name will be used for labels.
+#' @param time_order_col Optional numeric variable with values corresponding to order of values for \code{time_col}. Defaults to \code{"VISITNUM"}  
 #' @param time_order Option to specify order of x-axis using a character vector.
 #' @param time_rotate_tick_labels Rotate x-axis tick labels 45 degrees?  Defaults to \code{FALSE}.
 #' @param time_vertical_space X-axis padding for rotated labels in pixels. Defaults to \code{0}.
@@ -15,13 +16,16 @@
 #' @param normal_col_low   Variable name for column containing lower limit of normal values. Default is \code{"STNRLO"}.
 #' @param normal_col_high   Variable name for column containing upper limit of normal values. Default is \code{"STNRHI"}.
 #' @param start_value Value of variable defined in \code{measure_col} to be rendered in the plot when the widget loads. 
-#' @param groups_col Optional: name of column(s) to include as options for chart stratification.  Default is \code{"NONE"}.
-#' @param groups_label Optional label for stratification variable(s).  Default is \code{"None"}. If set to \code{NULL}, variable name will be used as label.
+#' @param groups_col Optional: name of column(s) to include as options for chart stratification.  Default is \code{NULL}.
+#' @param groups_label Optional label for stratification variable(s).  Default is \code{NULL}. If set to \code{NULL} and \code{groups_col} is specified, variable name will be used as label.
 #' @param filters_col Optional vector of variable names to use for filters
 #' @param filters_label Associated labels to use for filters. If left as \code{NULL}, variable names will be used as labels. 
 #' @param missingValues A vector of values found in \code{value_col} to be ignored when rendering the chat. Default is \code{c('','NA','N/A')}.
 #' @param boxplots Logical indicating whether to render boxplots.  Default is \code{TRUE}.
 #' @param violins Logical indicating whether to render violin plots.  Default is \code{FALSE}.
+#' @param visits_without_data Option to display visits without data. Defaults to \code{FALSE}.
+#' @param unscheduled_visits Option to display unscheduled visits (values of \code{time_col} containing "unscheduled" or "early termination").. Defaults to \code{FALSE}.
+#' @param width Width in pixels 
 #' @param width Width in pixels 
 #' @param height Height in pixels  
 #' @param elementId The element ID for the widget.
@@ -43,8 +47,9 @@
 #' @export
 safetyResultsOverTime <- function(data, 
                                   id_col = "USUBJID",
-                                  time_col = "VISITN",
-                                  time_label = "Visit Number",
+                                  time_col = "VISIT",
+                                  time_label = "Visit",
+                                  time_order_col = "VISITNUM",
                                   time_order = NULL,
                                   time_rotate_tick_labels =  FALSE,
                                   time_vertical_space = 0,
@@ -54,13 +59,15 @@ safetyResultsOverTime <- function(data,
                                   normal_col_low ="STNRLO", 
                                   normal_col_high = "STNRHI", 
                                   start_value = NULL,
-                                  groups_col = "NONE",
-                                  groups_label = "None",
+                                  groups_col = NULL,
+                                  groups_label = NULL,
                                   filters_col = NULL, 
                                   filters_label = NULL,
                                   missingValues = c('','NA','N/A'),
                                   boxplots = TRUE,
                                   violins = FALSE,
+                                  visits_without_data = FALSE,
+                                  unscheduled_visits = FALSE,
                                   width = NULL, height = NULL, elementId = NULL) {
 
   
@@ -69,11 +76,14 @@ safetyResultsOverTime <- function(data,
     time_settings <- list(value_col = time_col, 
                                 label = time_label, 
                                 order = as.character(time_order),
+                                order_col = time_order_col,
                                 rotate_tick_labels = time_rotate_tick_labels,
                                 vertical_space = time_vertical_space)
   } else{
     time_settings <- list(value_col = time_col, 
                                 label = time_label,
+                                order = time_order,
+                                order_col = time_order_col,
                                 rotate_tick_labels = time_rotate_tick_labels,
                                 vertical_space = time_vertical_space)
   }
@@ -93,6 +103,10 @@ safetyResultsOverTime <- function(data,
     filters <- data.frame(value_col = filters_col, label = filters_col)    
   }
   
+  # coerce NA to "NA"
+  if ('NA' %in% missingValues){
+    data[is.na(data)] <- "NA" 
+  }
   
   # forward options using x
   x = list(
@@ -111,7 +125,9 @@ safetyResultsOverTime <- function(data,
         start_value = start_value,
         missingValues = missingValues,
         boxplots = boxplots,
-        violins = violins
+        violins = violins,
+        visits_without_data = visits_without_data,
+        unscheduled_visits = unscheduled_visits 
       ),
       null="null", auto_unbox=T
     )
