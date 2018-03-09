5b5ab36efd10d4cb589bb1d650d328b961855987
bzkrouse <rebecca_zabel@rhoworld.com>
2018-03-09 11:57:52 -0500

    update docs for datasets

--- data-raw/ADAE.R
+++ data-raw/ADAE.R
@@ -2,6 +2,11 @@ URL <- "https://raw.githubusercontent.com/RhoInc/viz-library/master/data/safetyD
 download.file(url=URL,
               destfile='data-raw/ADAE.csv', method='auto')
 
-ADAE <- read.csv('data-raw/ADAE.csv')
+ADAE <- read.csv('data-raw/ADAE.csv', stringsAsFactors = FALSE)
+ADAE$ASEQ <- NULL 
+ADAE$AETERM[grep("pelvic floor", ADAE$AETERM)] <- "Weakness in the pelvic floor"
 
 save(ADAE, file = "data/ADAE.rdata")
+
+
+
